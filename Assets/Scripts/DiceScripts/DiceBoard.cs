7b28c5290bb769d09f334f4027c340cebac043d0
Marshall Brock <marshallbrock@Marshalls-Air.attlocal.net>
2021-10-10 13:23:18 -0500

    i can move around  a piece and it is confined within the bounds of the board

--- Assets/Scripts/DiceScripts/DiceBoard.cs
+++ Assets/Scripts/DiceScripts/DiceBoard.cs
@@ -10,14 +10,15 @@ public class DiceBoard : MonoBehaviour
         public Vector3Int spawnPosition;
         public Tilemap tilemap{get; private set;}
         public DiceGroup activeGroup {get; private set;}
-        //public Vector2Int boardSize= new Vector2Int(10,20);
-        // public RectInt Bounds {
-        //     get
-        //     {
-        //     Vector2Int position = new Vector2Int(-this.boardSize.x / 2, -this.boardSize.y / 2);
-        //     return new RectInt(position, this.boardSize);
-        //     }
-        // }   
+        //for is valid
+        public Vector2Int boardSize= new Vector2Int(6,9);//cells on the board
+        public RectInt Bounds {
+            get
+            {
+            Vector2Int position = new Vector2Int(-this.boardSize.x / 2, -this.boardSize.y / 2);
+            return new RectInt(position, this.boardSize);
+            }
+        }   
 
 
     // Start is called before the first frame update
@@ -26,7 +27,7 @@ public class DiceBoard : MonoBehaviour
         this.tilemap = GetComponentInChildren<Tilemap>();
         this.activeGroup = GetComponentInChildren<DiceGroup>();
 
-        //initialize all the possible options with there cells array
+        //initialize all the possible options with their cells array
         for(int i = 0;i<this.DiceOptions.Length;i++){
         this.DiceOptions[i].Initialize();
         }
@@ -44,9 +45,6 @@ public class DiceBoard : MonoBehaviour
         this.activeGroup.Initialize(this, spawnPosition, newGroup);
         SetOnBoard(this.activeGroup);//pass the dice group collection to be placed on the board
 
-                
-  
-
     }   
     // Update is called once per frame
     void Update()
@@ -57,14 +55,52 @@ public class DiceBoard : MonoBehaviour
     //place the cell/tilegroup in the active dice set on the board
     public void SetOnBoard(DiceGroup diceGroup)
     {
+        
         for(int i = 0;i< diceGroup.cells.Length;i++)
         {
+            //Debug.Log("SETTING TILE ON BOARD"+diceGroup.cells);
             //each tile in this dice group array is placed on the map
-            Vector3Int tilePosition = diceGroup.cells[i];//+ piece.position
+            Vector3Int tilePosition = diceGroup.cells[i]+ diceGroup.position;
             //what is the position it needs to be placed(more important if there is more than 1)/which tile to use
             //we have a position where we are falling/place what are the coordinates from that location do the other tiles need to go to fill in that shape
             this.tilemap.SetTile(tilePosition,diceGroup.data.tile);//
         }
 
+    }
+    
+    public void Clear(DiceGroup group)
+    {
+        for(int i = 0;i< group.cells.Length;i++)
+        {
+            Vector3Int tilePosition = group.cells[i] + group.position;
+            this.tilemap.SetTile(tilePosition,null);
+        }
+
+    }
+
+    public bool IsValidPosition(DiceGroup group, Vector3Int position)
+    {
+        RectInt bounds = this.Bounds;
+        // Debug.Log("Here are the bounds "+bounds);
+        // The position is only valid if every cell is valid
+        for (int i = 0; i < group.cells.Length; i++)
+        {
+            Vector3Int tilePosition = group.cells[i] + position;
+            Debug.Log("Here is the tile position "+ (Vector2Int)tilePosition);
+            // An out of bounds tile is invalid
+            if (!bounds.Contains((Vector2Int)tilePosition)) {
+                //Debug.LogError("bounds does not contain that position");
+                return false;
+            }
+
+            // A tile already occupies the position, thus invalid
+            if (this.tilemap.HasTile(tilePosition)) {
+                //Debug.LogError("tile map has that tile");
+                return false;
+            }
+        }
+
+        return true;
+        
     }
 }
--- Assets/Scripts/DiceScripts/DiceGroup.cs
+++ Assets/Scripts/DiceScripts/DiceGroup.cs
@@ -15,15 +15,17 @@ public class DiceGroup : MonoBehaviour
                             ///game board           spawn location      dice data currently active
     public void Initialize(DiceBoard diceBoard, Vector3Int position, DiceData data)
     {
+        Debug.Log("Spawn location:  "+ position);
         this.diceBoard = diceBoard;
         this.position = position;
         this.data = data;
 
 
-        //if we have initialized this array do it now
-        if(this.cells == null){
+        //if we have not initialized this array do it now
+        if(this.cells == null)
+        {
             //this.cells = new Vector3Int[data.cellLocation.Length];//single die
-                        this.cells = new Vector3Int[1];//single die
+            this.cells = new Vector3Int[1];//single die
 
         }
 
@@ -33,5 +35,42 @@ public class DiceGroup : MonoBehaviour
         }
 
 
+    }
+
+    void Update()
+    {
+        //MoveController();
+        this.diceBoard.Clear(this);
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Move(Vector2Int.left);
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Move(Vector2Int.right);
+        }
+        if(Input.GetKeyDown(KeyCode.DownArrow)){
+            Move(Vector2Int.down);
+        }
+        
+        this.diceBoard.SetOnBoard(this);
+    }
+
+    private bool Move(Vector2Int translation)
+    {
+        Vector3Int newPosition = this.position;
+        newPosition.x += translation.x;
+        newPosition.y += translation.y;
+
+        bool valid = this.diceBoard.IsValidPosition(this, newPosition);
+        Debug.Log("IS VALID?: " + valid);
+        // Only save the movement if the new position is valid
+        if (valid)
+        {
+            this.position = newPosition;
+            this.position = newPosition;
+        }
+
+        return valid;
     }
 }
