0f734cf4ec43f84d3e0ba53c01ed88d5ff67fad1
brockstar512 <60516979+brockstar512@users.noreply.github.com>
2022-01-22 11:16:17 -0600

    Sill need to finsh fx

--- Assets/FX Sprites Assets/DiceFXController.cs
+++ Assets/FX Sprites Assets/DiceFXController.cs
@@ -97,7 +97,6 @@ public class DiceFXController : MonoBehaviour
     //}
     public async Task FXTask(TileEffect effect, Vector3Int location)
     {
-        diceBoard.Clear(location);
         Effect effectConfig = ConfigureTile(effect);
         await effectConfig.AnimateTask(location, map);
     }
